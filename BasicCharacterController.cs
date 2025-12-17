8525e7fbe92a7a62b9428c5fea041d2dd6b35d92
phynon <tallusgreenland@outlook.com>
2025-12-17 23:39:37 +0800

    feat(chara): add walk anim states test

--- BasicCharacterController.cs
+++ BasicCharacterController.cs
@@ -3,8 +3,23 @@ using System;
 
 public partial class BasicCharacterController : CharacterBody3D
 {
-	public const float Speed = 5.0f;
+	public const float Speed = 1.0f;
 	public const float JumpVelocity = 4.5f;
+	public enum PlayerState
+	{
+		Idle,
+		Walking,
+		Jumping
+	}
+	PlayerState playerState;
+	
+	public AnimationTree _AnimationStateMachine;
+
+	public override void _Ready()
+	{
+		_AnimationStateMachine = GetNode<AnimationTree>("./AnimationTree");
+		_AnimationStateMachine.Active = true;
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -28,19 +43,33 @@ public partial class BasicCharacterController : CharacterBody3D
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
-			double _targetAngle = Mathf.Atan2(direction.X, direction.Z);
-			var meshNode = GetNode<Node3D>("bear");
-			meshNode.Rotation = new Vector3(meshNode.Rotation.X, (float)Mathf.LerpAngle(meshNode.Rotation.Y, _targetAngle, delta), meshNode.Rotation.Z);
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * 2 * Speed;
+			double _targetAngle = Mathf.Atan2(inputDir.X, inputDir.Y);
+			velocity.X = inputDir.X * Speed;
+			velocity.Z = inputDir.Y * 2 * Speed;
+			Rotation = new Vector3(Rotation.X, (float)Mathf.LerpAngle(Rotation.Y, _targetAngle + 0.50f * Mathf.Pi, 20.0f * delta), Rotation.Z);
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, 2 * Speed);
 		}
-
 		Velocity = velocity;
+		_AnimationUpdate();
+		GD.Print(playerState);
 		MoveAndSlide();
 	}
+	
+	public void _AnimationUpdate()
+	{
+		if (Velocity != Vector3.Zero)
+		{
+			playerState = PlayerState.Walking;
+		}
+		else
+		{
+			playerState = PlayerState.Idle;
+		}
+	}
+	
+	public PlayerState GetAnimationState() => playerState;
 }
